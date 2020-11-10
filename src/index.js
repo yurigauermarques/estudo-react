e29f58bfd499ef8c25c60d1153ade6a2f9c7992b
Yuri Gauer Marques <yurigmarques@gmail.com>
2020-11-09 20:52:23 -0400

    Componentes parte 1

--- src/index.js
+++ src/index.js
@@ -4,42 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-function formatName(user){
-  return user.firstName + ' ' + user.lastName;
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-const user = {
-  firstName: 'Yuri',
-  lastName: 'Guaer Marques'
-};
-
-const element = (
+function Appexemplo() {
+  return (
     <div>
-      <h1>
-        Hello, {formatName(user)}!
-      </h1>
-      <h2>Good to see you here.</h2>
+      <Welcome name="Yuri" />
+      <Welcome name="Grazi" />
+      <Welcome name="Panqueca" />
+      <Welcome name="Mingal" />
     </div>
-);
+  );
+}
+
 
 ReactDOM.render(
-  element,
+  <Appexemplo />,
   document.getElementById('root')
 );
 
-
-function tick(){
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
-}
-
-setInterval(tick, 1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
